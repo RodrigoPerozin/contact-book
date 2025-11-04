c09d86c7243156473d8417fe32c904a786316778
RodrigoPerozin <rodrigodperozin@gmail.com>
2025-11-04 01:36:09 -0300

    feat: change favicon.ico feat: new Welcome page texts feat: unexistant endpoints handler

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\ContactsController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
+//Just a casual API home page
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,4 +15,14 @@ Route::controller(ContactsController::class)->group(function () {
     Route::post('/api/contact', 'create'); // Create a new contact
     Route::put('/api/contact/{id}', 'update'); // Update a contact by ID
     Route::get('/api/contact/default-picture', 'defaultPicture'); // Get default contact picture
+});
+
+
+//Se não houver endpoint correspondente, retorna um erro 404 em formato JSON
+Route::fallback(function (Request $request) {
+    return response()->json([
+        'error' => 'Endpoint not found',
+        'path' => $request->path(),
+        'method' => $request->method(),
+    ], 404);
 });
